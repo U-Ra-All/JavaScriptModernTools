0b39d0be066b9662df00e31e034d894124e98946
YouRa <allakhverdovyuriy@gmail.com>
2021-12-11 11:48:26 +0200

    06-Модули CommonJS

--- script.js
+++ script.js
@@ -51,30 +51,36 @@
 // const lastPostData1 = await getLastPost();
 // console.log(lastPostData1);
 
-const ShoppingCart1 = (function () {
-  const cart = [];
-  const shippingCost = 20;
-  const totalPrice = 300;
-  const totalQuantity = 10;
-  const addProductToCart = function (product, quantity) {
-    cart.push({ product, quantity });
-    console.log(
-      `${product} в количестве ${quantity} шт добавлено в корзину, цена доставки ${shippingCost}`
-    );
-  };
+///////////////////////////////////////////////
+// Шаблон Проектирования Модуль
 
-  const productOrderedMessage = function (product, quantity) {
-    console.log(`${product} в количестве ${quantity} шт заказан`);
-  };
+// const ShoppingCart1 = (function () {
+//   const cart = [];
+//   const shippingCost = 20;
+//   const totalPrice = 300;
+//   const totalQuantity = 10;
+//   const addProductToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//       `${product} в количестве ${quantity} шт добавлено в корзину, цена доставки ${shippingCost}`
+//     );
+//   };
 
-  return {
-    addProductToCart,
-    cart,
-    totalPrice,
-    totalQuantity,
-  };
-})();
+//   const productOrderedMessage = function (product, quantity) {
+//     console.log(`${product} в количестве ${quantity} шт заказан`);
+//   };
 
-ShoppingCart1.addProductToCart('апельсин', 5);
-ShoppingCart1.addProductToCart('кола', 1);
-console.log(ShoppingCart1);
+//   return {
+//     addProductToCart,
+//     cart,
+//     totalPrice,
+//     totalQuantity,
+//   };
+// })();
+
+// ShoppingCart1.addProductToCart('апельсин', 5);
+// ShoppingCart1.addProductToCart('кола', 1);
+// console.log(ShoppingCart1);
+
+///////////////////////////////////////////////
+// Модули CommonJS
